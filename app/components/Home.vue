9e3fdfa086c3800293f9024d534ef8c694099165
Evgeny Pospelov <pospelov.genya@yandex.ru>
2023-05-03 23:05:27 +0500

    add Time and Weight pages

--- app/components/Home.vue
+++ app/components/Home.vue
@@ -1,27 +1,36 @@
 <template>
     <Page>
         <ActionBar>
-            <Label text="Home"/>
+            <Label text="Converter"/>
         </ActionBar>
 
-        <GridLayout>
-
+        <GridLayout rows="*, *" columns="*">
+            <Button v-for="page in pages" :key="page.id" :row="page.id" :text="page.name" @tap="Go(page.id)" />
         </GridLayout>
     </Page>
 </template>
 
 <script>
+import Time from './Time.vue';
+import Weight from './Weight.vue';
     export default {
         data () {
             return {
+                pages: [
+                    {id: 0, name: 'ToTime', value: Time},
+                    {id: 1, name: 'ToWeight', value: Weight},
+                ]
             };
         },
         methods: {
+            Go: function(id) {
+                this.$navigateTo(this.pages[id].value);
+            }
         }
     };
 </script>
 
 <style scoped lang="scss">
     @import '@nativescript/theme/scss/variables/blue';
-    
+    @import 'app/app.scss';
 </style>
---
+++ app/components/Weight.vue
@@ -0,0 +1,31 @@
+<template>
+    <Page>
+        <ActionBar>
+            <Label text="Weight"/>
+        </ActionBar>
+
+        <GridLayout rows="*" columns="*">
+            <Button text="ToHome" @tap="GoToHome()" />
+        </GridLayout>
+    </Page>
+</template>
+
+<script>
+import Home from './Home.vue';
+    export default {
+        data () {
+            return {
+            };
+        },
+        methods: {
+            GoToHome: function() {
+                this.$navigateTo(Home);
+            }
+        }
+    };
+</script>
+
+<style scoped lang="scss">
+    @import '@nativescript/theme/scss/variables/blue';
+    @import 'app/app.scss';
+</style>
