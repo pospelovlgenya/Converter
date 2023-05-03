28f4aefd1a0478777080469726113b5b9e4450c8
Evgeny Pospelov <pospelov.genya@yandex.ru>
2023-05-03 21:38:10 +0500

    Init commit

---
+++ app/components/Home.vue
@@ -0,0 +1,27 @@
+<template>
+    <Page>
+        <ActionBar>
+            <Label text="Home"/>
+        </ActionBar>
+
+        <GridLayout>
+
+        </GridLayout>
+    </Page>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+            };
+        },
+        methods: {
+        }
+    };
+</script>
+
+<style scoped lang="scss">
+    @import '@nativescript/theme/scss/variables/blue';
+    
+</style>
